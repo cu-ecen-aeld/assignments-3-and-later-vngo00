6748aa02c6614a710fd5fba1635598bdc8990030
vngo00 <phucvinh1131@gmail.com>
2023-08-27 11:42:30 -0700

    completed assignment 1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -13,6 +13,8 @@ then
 	exit 1
 fi
 
+
+
 X=$(find $1 -type f | wc -l)
 Y=$(grep -r $2 $1 | wc -l)
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -9,5 +9,8 @@ then
 	exit 1
 fi
 
+#create dir if not exists
+mkdir -p $( dirname $1)
+
 echo $2 > $1
 
